7119e91c22b2f15bf323a42b6ea8a7199a40aac2
paul <paul@dark-light.be>
2026-04-20 14:34:45 +0000

    File changes

--- src/pages/Templates.jsx
+++ src/pages/Templates.jsx
@@ -326,6 +326,7 @@ export default function Templates() {
               '{job_title}',
               '{month}',
               '{month_end_total_client_1} = excl. BTW',
+              '{month_end_total_client_ex_btw} = {1}',
               '{month_end_total_client_2} = incl. BTW',
               '{notice_period_client}',
               '{notice_period_consultant}',
